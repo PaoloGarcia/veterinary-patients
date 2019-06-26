9b52cfd7d375e6c863e69798d8145f3c4e198beb
PaoloGarcia <paolo.hernandezg@outlook.com>
2019-06-26 18:48:58 -0500

    last commit so far

--- src/components/ItemAppointment/ItemAppointment.js
+++ src/components/ItemAppointment/ItemAppointment.js
@@ -1,26 +1,80 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const ItemAppointment = ({
-    appointment: { pet, owner, date, time, symptoms },
-    onDeleteAppointment
-}) => {
+const ItemAppointment = ({ appointment, onDeleteAppointment }) => {
     return (
         <div className="media mt-3">
             <div className="media-body">
                 <h4 className="mt-0">
-                    {pet}
+                    {appointment.pet}
                 </h4>
-                <p className="card-text"><span>Pet Owner: </span>{owner}</p>
-                <p className="card-text"><span>Date: </span>{date}</p>
-                <p className="card-text"><span>Time: </span>{time}</p>
-                <p className="card-text"><span>Symptoms: </span>{symptoms}</p>
+                <p className="card-text"><span>Pet Owner: </span>{appointment.owner}</p>
+                <p className="card-text"><span>Date: </span>{appointment.date}</p>
+                <p className="card-text"><span>Time: </span>{appointment.time}</p>
+                <p className="card-text"><span>Symptoms: </span>{appointment.symptoms}</p>
 
-                <button
+                {/* <button
                     className="btn btn-danger"
-                    onClick={onDeleteAppointment}>
+                    data-toggle="modal"
+                    data-target="#deleteModal"
+                // onClick={onDeleteAppointment}
+                >
                     Delete Appointment
                 </button>
+
+                <div
+                    className="modal"
+                    id="deleteModal"
+                    tabIndex="-1"
+                    role="dialog"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                >
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Modal title</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <p>Modal body text goes here.</p>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-primary">Delete</button>
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
+
+
+
+                <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                    Launch demo modal
+                </button>
+
+                <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                ...
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                <button type="button" className="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
